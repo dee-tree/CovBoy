0ea70bdb22e6ba3c41dedb59e05065b388d39d71
dmitriy.sokolov <codemitry@gmail.com>
2022-07-20 13:48:08 +0300

    allModels changes, added Expr - walk method

--- src/main/kotlin/com/microsoft/z3/coverage/models.kt
+++ src/main/kotlin/com/microsoft/z3/coverage/models.kt
@@ -1,16 +1,15 @@
 package com.microsoft.z3
 
 
-fun Solver.allModels(): Set<Model> = buildSet {
+fun Solver.allModels(limit: Int = 10): Set<Model> = buildSet {
 
     var i = 0
-    val limit = 10
     whileSat { model ->
         if (i > limit) return@whileSat context.mkFalse()
-            add(model)
-            val modelExprs = model.constDecls.map { decl ->
-                (context.mkConst(decl) eq model.getConstInterp(decl))
-            }
+        add(model)
+        val modelExprs = model.constDecls.map { decl ->
+            (context.mkConst(decl) eq model.getConstInterp(decl))
+        }
 
         i++
 
@@ -18,15 +17,30 @@ fun Solver.allModels(): Set<Model> = buildSet {
     }
 }
 
-fun Optimize.allModels(): Set<Model> = buildSet {
-    whileSat { model ->
 
-        add(model)
-        val modelExprs = model.constDecls.map { decl ->
-            (context.mkConst(decl) eq model.getConstInterp(decl))
+fun Expr.walk(action: (Expr) -> Boolean) {
+    if (!action(this))
+        return
+    when {
+        isApp -> {
+            args.forEach {
+                it.walk(action)
+            }
         }
 
-        !context.and(*modelExprs.toTypedArray())
-    }
+        isQuantifier -> {
+            println("quantifier: $this")
+        }
 
+        isVar -> {
+            println("var: $this")
+        }
+    }
 }
+
+val Model.constDeclsAsExpressions: List<Expr>
+    get() = buildList {
+        constDecls.forEach { decl ->
+            add(context.mkConst(decl) eq getConstInterp(decl))
+        }
+    }
